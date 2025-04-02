a6c262d7e6841e8e009423c64f95451d63a61507
abeni-csa <abenezerhaymanot@gmail.com>
2025-04-02 13:10:30 +0300

    fix compiler warings and fix more formatings on print

--- src/examle.rs
+++ src/examle.rs
@@ -2,13 +2,13 @@ fn add_one_v1(x: u32) -> u32 {
     x + 1
 }
 pub fn call_me() {
-    let add_one_v1 = |x: u32| -> u32 { x + 1 };
+    let _add_one_v1 = |x: u32| -> u32 { x + 1 };
     let example_closure = |y| y;
-    let str = example_closure(String::from("Some String"));
-    let list = vec![1, 2, 3, 4];
+    let _str = example_closure(String::from("Some String"));
+    let mut list = vec![1, 2, 3, 4];
+    add_one_v1(1);
     print!("Before defining the closure: {list:?}");
-    let only_borrows = || println!("From clousure: {list:?}");
-    println!("berfore calling clousure: {list:?}");
-    only_borrows();
+    let mut borrows_mutable = || list.push(7);
+    borrows_mutable();
     println!("After calling clousure: {list:?}");
 }
