fab169fd4a01694b95b00fc3bab0ecd86273d063
abeni-csa <abenezerhaymanot@gmail.com>
2025-04-02 12:53:18 +0300

    stil no idea how to play with closure

--- src/examle.rs
+++ src/examle.rs
@@ -1,3 +1,14 @@
+fn add_one_v1(x: u32) -> u32 {
+    x + 1
+}
 pub fn call_me() {
-    todo!()
+    let add_one_v1 = |x: u32| -> u32 { x + 1 };
+    let example_closure = |y| y;
+    let str = example_closure(String::from("Some String"));
+    let list = vec![1, 2, 3, 4];
+    print!("Before defining the closure: {list:?}");
+    let only_borrows = || println!("From clousure: {list:?}");
+    println!("berfore calling clousure: {list:?}");
+    only_borrows();
+    println!("After calling clousure: {list:?}");
 }
